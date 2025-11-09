cbfd56b2509a10110e8e5bad4dbd8cd8b1941f1a
Rosalie Vicente <rjivicente@outlook.com>
2025-11-10 04:19:31 +0800

    .002

---
+++ src/cozybrew/ui/AssetLoader.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cozybrew.ui;
+
+/**
+ *
+ * @author Rosalie Joy Indelible Vicente
+ */
+public class AssetLoader {
+    
+}
