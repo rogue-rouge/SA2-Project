f0266e5f990a47271462c22b23418c197b3901da
Kirsten Iguet <mkd.iguet@gmail.com>
2025-11-11 03:57:24 +0800

    Update RoundedButton.java

--- src/cozybrew/ui/panels/RoundedButton.java
+++ src/cozybrew/ui/panels/RoundedButton.java
@@ -1,19 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package cozybrew.ui.panels;
+
+/**
+ *
+ * @author Kirs Iguet
+ */
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.RoundRectangle2D;
+import javax.swing.JButton;
 
 /**
  * to make the buttons look nicer 
  */
 public class RoundedButton extends JButton {
 
-    private Color backgroundColor = new Color(101, 67, 33, 200);
-    private Color hoverColor = new Color(131, 97, 63, 200);
+    private Color baseColor = new Color(139, 94, 56, 200); // R:139 G:94 B:56, 80% opaque
+    private Color hoverColor = new Color(159, 114, 76, 200); // Slightly lighter for hover
+    private Color pressedColor = new Color(119, 74, 36, 200); // Slightly darker for pressed
     private int cornerRadius = 25;
 
-    public RoundedButton(String text) {
+public RoundedButton(String text) {
         super(text);
         
         setContentAreaFilled(false);
@@ -33,11 +44,11 @@ public class RoundedButton extends JButton {
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
         if (getModel().isPressed()) {
-            g2.setColor(backgroundColor.darker());
+            g2.setColor(pressedColor);
         } else if (getModel().isRollover()) {
             g2.setColor(hoverColor);
         } else {
-            g2.setColor(backgroundColor);
+            g2.setColor(baseColor);
         }
         
         g2.fill(new RoundRectangle2D.Float(0, 0, getWidth(), getHeight(), cornerRadius, cornerRadius));
@@ -47,3 +58,4 @@ public class RoundedButton extends JButton {
         g2.dispose();
     }
 }
+
