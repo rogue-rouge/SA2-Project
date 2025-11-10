271aa3f6248184172f20f92fc9a72b705a9f80f8
Kirsten Iguet <mkd.iguet@gmail.com>
2025-11-10 20:33:20 +0800

    Update AssetLoader.java

--- src/cozybrew/ui/AssetLoader.java
+++ src/cozybrew/ui/AssetLoader.java
@@ -8,6 +8,87 @@ package cozybrew.ui;
  *
  * @author Rosalie Joy Indelible Vicente
  */
+import javax.swing.ImageIcon;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+
 public class AssetLoader {
+    private Image backgroundImage;
+    private ImageIcon[] animationFrames;
+    private ImageIcon sugarCubeIcon;
+
+    public AssetLoader() {
+        loadBackgroundImage();
+        loadAnimationFrames();
+        loadButtonIcons();
+    }
+
+    private void loadBackgroundImage() {
+        try {
+            this.backgroundImage = ImageIO.read(getClass().getResource("/cozybrew/img/brewbg.png"));
+        } catch (Exception e) {
+            System.err.println("Failed to load /cozybrew/img/brewbg.png. Using fallback gradient.");
+            e.printStackTrace();
+            this.backgroundImage = null;
+        }
+    }
+
+    private void loadAnimationFrames() {
+        String[] frameNames = {
+            "brew1.png",
+            "brew2.png",
+            "brew3.png",
+            "brew4.png"
+        };
+        
+        this.animationFrames = new ImageIcon[frameNames.length];
+        
+        for (int i = 0; i < frameNames.length; i++) {
+            String path = "/cozybrew/img/" + frameNames[i];
+            try {
+                this.animationFrames[i] = new ImageIcon(getClass().getResource(path));
+            } catch (Exception e) {
+                System.err.println("Failed to load animation frame: " + path);
+                this.animationFrames[i] = createPlaceholderIcon("Failed: " + frameNames[i], 100, 100);
+            }
+        }
+    }
+
+    private void loadButtonIcons() {
+        try {
+            this.sugarCubeIcon = new ImageIcon(getClass().getResource("/cozybrew/img/sugar_cube.png"));
+        } catch (Exception e) {
+            System.err.println("Failed to load /cozybrew/img/sugar_cube.png. Using placeholder.");
+            this.sugarCubeIcon = createPlaceholderIcon("Icon", 32, 32);
+        }
+    }
     
+
+    public Image getBackgroundImage() {
+        return this.backgroundImage;
+    }
+
+    public ImageIcon[] getAnimationFrames() {
+        return this.animationFrames;
+    }
+    
+    public ImageIcon getSugarCubeIcon() {
+        return this.sugarCubeIcon;
+    }
+
+    private ImageIcon createPlaceholderIcon(String text, int width, int height) {
+        java.awt.image.BufferedImage img = new java.awt.image.BufferedImage(width, height, java.awt.image.BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = img.createGraphics();
+        g2d.setColor(new Color(255, 255, 255, 50));
+        g2d.fillRect(0, 0, width, height);
+        g2d.setColor(Color.WHITE);
+        g2d.drawRect(0, 0, width - 1, height - 1);
+        g2d.setFont(new Font("SansSerif", Font.PLAIN, 10));
+        g2d.drawString(text, 5, height / 2);
+        g2d.dispose();
+        return new ImageIcon(img);
+    }
 }
