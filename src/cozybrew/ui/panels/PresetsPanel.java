b95219abe7664aa1ea45094d2aed3839fdf68b35
Kirsten Iguet <mkd.iguet@gmail.com>
2025-11-11 18:58:34 +0800

    Update PresetsPanel.java

--- src/cozybrew/ui/panels/PresetsPanel.java
+++ src/cozybrew/ui/panels/PresetsPanel.java
@@ -9,6 +9,7 @@ package cozybrew.ui.panels;
  * @author GROUP 3: JOHANES KINNARD COTE, MACAYLE THERESE DANCEL, MARY KIRSTEN DANIELLE IGUET, NESTOR JOSH BACANI, & ROSALIE JOY VICENTE
  */
 
+import cozybrew.audio.SoundEffectPlayer; 
 import cozybrew.logic.TimerEngine;
 import javax.swing.*;
 import java.awt.*;
@@ -25,28 +26,30 @@ import java.util.Map;
 public class PresetsPanel extends RoundedPanel {
     
     private final TimerEngine timer;
+    private final SoundEffectPlayer sfxPlayer;
     private Map<String, Integer> presets;
 
-    public PresetsPanel(TimerEngine timer) {
-            super(40, new Color(101, 67, 33, 200)); 
-            
-            this.timer = timer;
+    public PresetsPanel(TimerEngine timer, SoundEffectPlayer sfxPlayer) {
+        super();
+        this.timer = timer;
+        this.sfxPlayer = sfxPlayer;
+        
+        setLayout(new GridLayout(7, 1, 0, 10));
 
-            setLayout(new GridLayout(0, 1, 10, 10));
-            setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        Map<String, Integer> presets = new LinkedHashMap<>();
+        presets.put("1 Min", 60);
+        presets.put("5 Min", 5 * 60);
+        presets.put("10 Min", 10 * 60);
+        presets.put("15 Min", 15 * 60);
+        presets.put("30 Min", 30 * 60);
+        presets.put("45 Min", 45 * 60);
+        presets.put("1 Hour", 60 * 60);
 
-            presets = new LinkedHashMap<>();
-            presets.put("1 Min", 60);
-            presets.put("5 Min", 5 * 60);
-            presets.put("10 Min", 10 * 60);
-            presets.put("15 Min", 15 * 60);
-            presets.put("30 Min", 30 * 60);
-            presets.put("45 Min", 45 * 60);
-            presets.put("1 Hour", 60 * 60);
-      
-            for (final Map.Entry<String, Integer> entry : presets.entrySet()) {
+        for (final Map.Entry<String, Integer> entry : presets.entrySet()) {
             JButton btn = new RoundedButton(entry.getKey());
+            
             btn.addActionListener(e -> {
+                sfxPlayer.playSound("click");
                 timer.setDurationSeconds(entry.getValue());
             });
             add(btn);
