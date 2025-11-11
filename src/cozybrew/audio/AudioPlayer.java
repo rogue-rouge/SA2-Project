001eb3168feb6b33f9508e705fb4d60cc54fd05c
Kirsten Iguet <mkd.iguet@gmail.com>
2025-11-11 21:16:57 +0800

    Update AudioPlayer.java

--- src/cozybrew/audio/AudioPlayer.java
+++ src/cozybrew/audio/AudioPlayer.java
@@ -8,14 +8,20 @@ package cozybrew.audio;
  *
  * @author GROUP 3: JOHANES KINNARD COTE, MACAYLE THERESE DANCEL, MARY KIRSTEN DANIELLE IGUET, NESTOR JOSH BACANI, & ROSALIE JOY VICENTE
  */
+
 import javax.sound.sampled.*;
 import java.io.IOException;
 import java.net.URL;
 
-public interface AudioPlayer {
+/**
+ * A simple audio player for handling background music.
+ * Loads a .wav file and can toggle it on (looping) and off (stop).
+ */
+public class AudioPlayer {
+
     private Clip clip;
     private boolean isPlaying = false;
-    
+
     /**
      * Loads the audio clip from the given resource path.
      * @param resourcePath The path to the audio file (e.g., "/resources/audio/lofi.wav")
@@ -26,8 +32,10 @@ public interface AudioPlayer {
             if (audioUrl == null) {
                 throw new IOException("Audio file not found: " + resourcePath);
             }
-        AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioUrl);
-clip = AudioSystem.getClip();
+
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioUrl);
+
+            clip = AudioSystem.getClip();
             clip.open(audioStream);
 
         } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
@@ -56,7 +64,7 @@ clip = AudioSystem.getClip();
             isPlaying = true;
         }
     }
-    
+
     /**
      * Stops the music permanently (e.g., when closing the app).
      */
